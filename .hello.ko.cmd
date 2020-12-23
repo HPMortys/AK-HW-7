4cc0955a016b4d5f35a0ecd31c47cb2a314c0901
HPMortys <55454592+HPMortys@users.noreply.github.com>
2020-12-23 13:28:52 +0200

    Delete .hello.ko.cmd

--- .hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/denis/Desktop/lab7/hello.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/denis/Desktop/lab7/hello.ko /home/denis/Desktop/lab7/hello.o /home/denis/Desktop/lab7/hello.mod.o;  true
